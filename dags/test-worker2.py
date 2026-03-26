8b7e37ba8b21b00a462986fec914d6f287f33522
sasha-production <sasha.ignatov93@gmail.com>
2026-03-26 18:05:56 +0500

    test github actions

---
+++ dags/test-worker2.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    'test_worker',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False
+) as dag:
+    task = BashOperator(
+        task_id='test_task',
+        bash_command='echo "Worker is working! Hostname: $(hostname)"'
+    )
